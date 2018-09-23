2d405d2ee29c26374d60721574e9a23e6cfb7ef9
Ali Naeem <alitheflyingtiger111@gmail.com>
2018-09-23 19:43:03 +0500

    Header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="wrapper">Hello World</div>
+        <div className="wrapper">
+          <Header />
+        </div>
       </div>
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  state = {};
+  render() {
+    return <h1>Header</h1>;
+  }
+}
