5f43cff63936c7484099c4de385e362f296b3ad7
Ali Naeem <alitheflyingtiger111@gmail.com>
2018-09-23 19:56:26 +0500

    TodoEnter added for todo entry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import TodoEnter from "./components/TodoEnter";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="wrapper">
           <Header />
+          <TodoEnter />
         </div>
       </div>
     );
---
+++ src/components/TodoEnter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class TodoEnter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: "test" };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  handleChange() {
+    console.log("you change something");
+  }
+
+  addTodo(todo) {
+    console.log("Todo: ", todo);
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" value="" onChange="{this.handleChange}" />
+        <button
+          className="btn btn-primary"
+          onClick={() => this.addTodo(this.state.value)}
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
